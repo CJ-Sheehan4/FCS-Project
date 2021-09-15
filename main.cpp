5806b49a2d456b9e681ffc658ff4e576757de102
CJ Sheehan <SheehanCorn@gmail.com>
2021-09-14 20:11:39 -0400

    updated task #1/changed data types

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
-#include <vector>
-#include "Character.h"
+#include <list>
 using namespace std;
 
 int main() {
-	
-	Character cOne(1);
-	Character cTwo(2);
-	Character cThree(3);
-
-	cout << "Test: constructors" << endl;
-	vector<Character> alphabet = {Character(), cOne, cTwo, cThree};
-	int size = alphabet.size();
-	for (int i = 0; i < size; i++) {
-		alphabet[i].print();
-	}
-
-	cout << endl << "Test: getter" << endl;
-	int firstVal = alphabet[0].getCharacter();
-	cout << firstVal << endl;
-	cout << "Test: setter" << endl;
-	alphabet[0].setCharacter(9);
-	cout << alphabet[0].getCharacter() << endl;
-	
+	//Task #1
+	list<int> sigma = { 0, 1 };
 
 	return 0;
 }
--- readme.txt
+++ readme.txt
@@ -1,6 +1,9 @@
 CJ Sheehan
 
 Task #1:
-my alphabets are going to be represented as a vector of Character obejcts
-The Character class definition can be found in Character.h along with the function definitions in Character.cpp
-I have a main.cpp to test Character.
+(updated)
+I realized I was overcomplicating things after reviewing the lectures. I decided to change my data 
+types for alphabet and characters. my alphabets are going to be represented as a list container/data 
+structure in the C++ library. The characters will be represented as integers.
+
+
