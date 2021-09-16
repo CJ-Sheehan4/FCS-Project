6a4cd9e101fac506469255699ff0cb69a6a06c8b
CJ Sheehan <SheehanCorn@gmail.com>
2021-09-16 16:20:08 -0400

    made some data type changes, attempted layer function, didnt succeed

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
 #include <list>
 using namespace std;
-list<int> layer(list<int> sigma, int N);
+list<list<int>> getLayer(list<list<int>> layerOne, list<int> sigma, int N);
+void printLayer(list<list<int>> layer);
 int main() {
 	//Task #1
-	list<int> sigma = { 0, 1 };
+	list<int> sigma = { 0, 1 };	// -1 represents epsilon, the alphabet consists of positive integers, including 0
 	//Task #2 - using a list to represent a string 
-	list<int> str = layer(sigma, 1);
+	list<list<int>> layerOne;
+	layerOne = getLayer(layerOne, sigma, 2);
+	printLayer(layerOne);
 	return 0;
 }
-list<int> layer(list<int> sigma, int N) {
-	return sigma;
+list<list<int>> getLayer(list<list<int>> layer, list<int> sigma, int N) {
+	list<int>::iterator i;
+	list<list<int>>::iterator j;
+	list<int> epsilon = { -1 };
+	int count = 0;
+	layer.push_back(epsilon);
+	while (count < N) {
+		if (count == 1) {
+			for (i = sigma.begin(); i != sigma.end(); i++) {
+				list<int> temp;
+				temp.push_back(*i);
+				layer.push_back(temp);
+			}
+		}
+		else {
+			
+		}
+		count++;
+	}
+	
+	return layer;
+}
+void printLayer(list<list<int>> layer) {
+	list<list<int>>::iterator rowIt;
+	list<int>::iterator columnIt;
+	for (rowIt = layer.begin(); rowIt != layer.end(); rowIt++) {
+		for (columnIt = rowIt->begin(); columnIt != rowIt->end(); columnIt++) {
+			cout << *columnIt << " ";
+		}
+		cout << " | ";
+	}
 }
