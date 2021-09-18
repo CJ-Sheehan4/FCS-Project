b1ae7f75d3451adb18c70e0b73937168d4bc839e
CJ Sheehan <SheehanCorn@gmail.com>
2021-09-18 13:13:08 -0400

    worked on task 3. implemented iterative layer function.

--- main.cpp
+++ main.cpp
@@ -8,39 +8,66 @@ int main() {
 	list<int> sigma = { 0, 1 };	// -1 represents epsilon, the alphabet consists of positive integers, including 0
 	//Task #2 - using a list to represent a string 
 	list<list<int>> layerOne;
-	layerOne = getLayer(layerOne, sigma, 2);
+	layerOne = getLayer(layerOne, sigma, 4);
 	printLayer(layerOne);
 	return 0;
 }
 list<list<int>> getLayer(list<list<int>> layer, list<int> sigma, int N) {
 	list<int>::iterator i;
 	list<list<int>>::iterator j;
+	list<int>::iterator sigmaIt;
+	list<list<list<int>>> uniLayers;
+	list<list<int>> newLayer;
+	list<int>temp;
 	list<int> epsilon = { -1 };
 	int count = 0;
 	layer.push_back(epsilon);
-	while (count < N) {
+	uniLayers.push_back(layer);
+	
+	while (count <= N) {
 		if (count == 1) {
+			layer.clear();
 			for (i = sigma.begin(); i != sigma.end(); i++) {
-				list<int> temp;
 				temp.push_back(*i);
 				layer.push_back(temp);
+				temp.clear();
 			}
+			uniLayers.push_back(layer);
 		}
-		else {
-			
+		else if(count > 1){
+			layer.clear();
+			layer = uniLayers.back();
+			for (sigmaIt = sigma.begin(); sigmaIt != sigma.end(); sigmaIt++) {
+				for (j = layer.begin(); j != layer.end(); j++) {
+					for (i = j->begin(); i != j->end(); i++) {
+						temp.push_back(*i);
+					}
+					temp.push_front(*sigmaIt);
+					newLayer.push_back(temp);
+					temp.clear();
+				}
+			}
+			uniLayers.push_back(newLayer);
+			newLayer.clear();
 		}
 		count++;
 	}
-	
-	return layer;
+	return uniLayers.back();
 }
 void printLayer(list<list<int>> layer) {
 	list<list<int>>::iterator rowIt;
 	list<int>::iterator columnIt;
 	for (rowIt = layer.begin(); rowIt != layer.end(); rowIt++) {
+		cout << "{";
 		for (columnIt = rowIt->begin(); columnIt != rowIt->end(); columnIt++) {
-			cout << *columnIt << " ";
+			cout << *columnIt;
+			if (columnIt == --rowIt->end()) {
+				break;
+			}
+			else {
+				cout << ", ";
+			}
 		}
-		cout << " | ";
+		cout << "}";
 	}
 }
